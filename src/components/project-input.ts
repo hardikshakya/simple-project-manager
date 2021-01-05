7351dfb69ec16932600f824f5c68706368d92455
hardikshakya <hardikshakya1997@gmail.com>
2021-01-06 01:26:26 +0530

    T8: Structure Code In Modules

---
+++ src/components/project-input.ts
@@ -0,0 +1,88 @@
+import Cmp from './base-component.js';
+import * as Validation from '../util/validation.js';
+import { autobind as Autobind } from '../decorators/autobind.js';
+import { projectState } from '../state/project-state.js';
+
+// ProjectInput Class
+export class ProjectInput extends Cmp<HTMLDivElement, HTMLFormElement> {
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    super('project-input', 'app', true, 'user-input');
+
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+
+    this.configure();
+  }
+
+  // add listener on submit
+  public configure() {
+    this.element.addEventListener('submit', this.submitHandler);
+  }
+
+  public renderContent() {}
+
+  // fetch user inputs
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    const titleValidatable: Validation.Validatable = {
+      value: enteredTitle,
+      required: true,
+    };
+    const descriptionValidatable: Validation.Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 3,
+    };
+    const peopleValidatable: Validation.Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+    };
+
+    if (
+      !Validation.validate(titleValidatable) ||
+      !Validation.validate(descriptionValidatable) ||
+      !Validation.validate(peopleValidatable)
+    ) {
+      alert('Invalid input, please try again!');
+      return;
+    }
+    return [enteredTitle, enteredDescription, +enteredPeople];
+  }
+
+  // clear form field after submit
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
+  // process user entered inputs
+  @Autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+
+    const userInput = this.gatherUserInput();
+
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      projectState.addProject(title, description, people);
+
+      this.clearInputs();
+    }
+  }
+}
